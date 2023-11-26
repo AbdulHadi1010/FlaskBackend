6beeb1c1a742608bd7915ff2672e0d23d455aa5b
Abdul Hadi <hadi2408466@gmail.com>
2023-11-26 16:46:13 +0500

    Push

--- api/app.py
+++ api/app.py
@@ -1,67 +1,60 @@
 from flask import Flask, request, jsonify
-# from pymongo import MongoClient
-# from pymongo.errors import PyMongoError
-# from bson import ObjectId
+import os
+from pymongo import MongoClient
+from pymongo.errors import PyMongoError
+from bson import ObjectId
 
 app = Flask(__name__)
-
-
+MONGO_URI = os.environ.get('mongodb+srv://AbdulHadi:khizer007@bigdata.jkwutqo.mongodb.net/?retryWrites=true&w=majority')
+client = MongoClient(MONGO_URI)
+db = client.get_database("BigData")
+collection = db.get_collection("Users")
 
 @app.route('/')
 def home():
     return 'Hello, World!'
 
-@app.route('/about')
-def about():
-    return 'About'
+@app.route('/process_data')
+def process_data():
+     return 'Hello, World!'
+    # try: , methods=['POST']
+    #     data = request.get_json()
+    #     print("Received JSON data:", data)
 
-# Configure the MongoDB connection URI
-# uri = 'mongodb+srv://AbdulHadi:khizer007@bigdata.jkwutqo.mongodb.net/?retryWrites=true&w=majority'
-# client = MongoClient(uri)
+    #     # Retrieve data fields
+    #     name = data.get('name')
+    #     age = data.get('age')
+    #     number = data.get('number')
+    #     print(f"Received data - Name: {name}, Age: {age}, Number: {number}")
 
-# # Select the database and collection
-# db = client.get_database("BigData")
-# collection = db.get_collection("Users")
+    #     # Construct MongoDB record
+    #     record = {
+    #         'name': name,
+    #         'age': age,
+    #         'number': number
+    #     }
+    #     print("Constructed Record:", record)
+    #     # Insert data into MongoDB
+    #     result = collection.insert_one(record)
 
-# @app.route('/process_data', methods=['POST'])
-# def process_data():
-#     try:
-#         data = request.get_json()
-#         print("Received JSON data:", data)
+    #     return jsonify({"result": "Success: Data inserted into MongoDB!", "inserted_id": str(result.inserted_id)})
+    # except PyMongoError as e:
+    #     return jsonify({"result": f"Failure: {str(e)}"})
 
-#         # Retrieve data fields
-#         name = data.get('name')
-#         age = data.get('age')
-#         number = data.get('number')
-#         print(f"Received data - Name: {name}, Age: {age}, Number: {number}")
+@app.route('/search_user')
+def search_user():
+      return 'Hello, World!'
+    # name = request.args.get('name')
 
-#         # Construct MongoDB record
-#         record = {
-#             'name': name,
-#             'age': age,
-#             'number': number
-#         }
-#         print("Constructed Record:", record)
-#         # Insert data into MongoDB
-#         result = collection.insert_one(record)
+    # # Search for the user by name , methods=['GET']
+    # user = collection.find_one({'name': name})
 
-#         return jsonify({"result": "Success: Data inserted into MongoDB!", "inserted_id": str(result.inserted_id)})
-#     except PyMongoError as e:
-#         return jsonify({"result": f"Failure: {str(e)}"})
+    # if user:
+    #     # Convert ObjectId to string
+    #     user['_id'] = str(user['_id'])
+    #     return jsonify({"result": "Success: User found!", "user": user})
+    # else:
+    #     return jsonify({"result": "Error: User not found."})
 
-# @app.route('/search_user', methods=['GET'])
-# def search_user():
-    name = request.args.get('name')
-
-    # Search for the user by name
-    user = collection.find_one({'name': name})
-
-    if user:
-        # Convert ObjectId to string
-        user['_id'] = str(user['_id'])
-        return jsonify({"result": "Success: User found!", "user": user})
-    else:
-        return jsonify({"result": "Error: User not found."})
-
-# if __name__ == '__main__':
-#     app.run()
+if __name__ == '__main__':
+    app.run()
